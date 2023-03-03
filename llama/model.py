6612c9976e883b250b747bc2e412a7ca6b2425b0
Fares Abawi <fabawi@users.noreply.github.com>
2023-03-03 23:11:23 +0100

    Update model.py

--- llama/model.py
+++ llama/model.py
@@ -1,5 +1,6 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the GNU General Public License version 3.
+# Modified by Fares Abawi (fares.abawi@uni-hamburg.de)
 
 from typing import Optional, Tuple
 from dataclasses import dataclass
